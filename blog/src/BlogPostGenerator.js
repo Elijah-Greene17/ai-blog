6f33e5a16ff4a8d533b923db4c7b22cd3061a57b
Elijah-Greene17 <egreene017@gmail.com>
2023-03-23 12:41:30 -0400

    Florida Work

--- blog/src/BlogPostGenerator.js
+++ blog/src/BlogPostGenerator.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import "./style.css";
 import { generate } from "./ai"
 
@@ -6,25 +6,144 @@ function BlogPostGenerator() {
   const [topic, setTopic] = useState("");
   const [keyWords, setKeyWords] = useState("");
   const [tone, setTone] = useState("");
-  const [maxLength, setMaxLength] = useState(2000);
+  const [maxLength, setMaxLength] = useState(-1);
   const [content, setContent] = useState("");
-  const generatePost = () => {
-    // Connect to Chat GPT API and generate post based on parameters
-    // Update state with generated post
-  };
+  const [contentIsLoading, setContentIsLoading] = useState(false)
+  const [contentIsWriting, setContentIsWriting] = useState(false)
+
+  // AI control Feaqtures
+  const [temp, setTemp] = useState(0.1)
+  const [max_tok, setMaxTok] = useState(2000)
+  const [freq, setFreq] = useState(0)
+  const [pres, setPres] = useState(0.5)
+
+  //Wordpress Markdown Feature
+  const [isWordPressMarkdown, setIsWordPressMarkdown] = useState(false)
+  const [showModal, setShowModal] = useState(false);
+
+  const textareaRef = useRef()
+  const modalRef = React.useRef();
+
+  function openModal() {
+    setShowModal(true);
+    document.body.style.overflow = "hidden";
+  }
+
+  function closeModal() {
+    setShowModal(false);
+    document.body.style.overflow = "auto";
+  }
+
+  function handleChange(event, setter) {
+    setter(event.target.value);
+  }
 
   async function handleWrite() {
-    // let ai = new OpenAI()
-    // ai.generateText("Write a blog post about machine learning in the tone of a pirate", "text-davinci-003", 2000)
-    console.log("Writing...")
-    let prompt = `Write a blog post about ${topic} in a ${tone} tone with a max length of ${maxLength} words with the keywords: ${keyWords.length > 0 ? keyWords : ""}`;
-    let res = await generate(prompt);
-    console.log(res)
-    setContent(res)
+    let p = ""
+    if (topic) {
+      p += "write a blog post about " + topic
+    }
+    if (tone) {
+      p += " in a " + tone + " tone"
+    }
+    if (maxLength > 0) {
+      p += " with a max length of " + maxLength + " words"
+    }
+    if (keyWords.length > 0) {
+      p += " with the keywords: " + keyWords
+    }
+
+    // Wordpress Markdown Feature
+    if (isWordPressMarkdown) {
+      p += ". Include markdown for wordpress."
+    }
+
+    const obj = {
+      temperature: temp,
+      max_tokens: Math.floor(max_tok),
+      frequency_penalty: freq,
+      presence_penalty: pres
+    }
+
+    console.log("Obj: ", obj)
+
+    setContentIsWriting(true)
+    setContentIsLoading(true)
+    let res = await generate(p, obj);
+
+    var i = 0;
+    var txt = res;
+    var speed = 10;
+    var total = ""
+
+    typeWriter()
+
+    function typeWriter() {
+      if (i < txt.length) {
+        total += txt.charAt(i);
+        setContent(total)
+        speed = Math.floor(Math.random() * 21) + 5;
+        const { current: textarea } = textareaRef;
+        if (textarea) {
+          textarea.scrollTop = textarea.scrollHeight;
+        }
+        i++;
+        setTimeout(typeWriter, speed);
+      }
+      else {
+        setContentIsWriting(false)
+      }
+    }
+
+    setContentIsLoading(false)
+
+  }
+
+  function handleDropdownChange(e) {
+    const value = e.target.value;
+    setMaxLength(value)
   }
 
   return (
     <div className="main">
+      {showModal && (
+        <div className="modal" ref={modalRef}>
+          <div className="modal-content">
+            <form>
+              <label>
+                Temperature:
+                <input type="text" value={temp} onChange={(event) => handleChange(event, setTemp)} />
+              </label>
+              <br />
+              <label>
+                Maximum Tokens:
+                <input type="text" value={max_tok} onChange={(event) => handleChange(event, setMaxTok)} />
+              </label>
+              <br />
+              <label>
+                Frequency Penalty:
+                <input type="text" value={freq} onChange={(event) => handleChange(event, setFreq)} />
+              </label>
+              <br />
+              <label>
+                Presence Penalty:
+                <input type="text" value={pres} onChange={(event) => handleChange(event, setPres)} />
+              </label>
+              <br />
+              <button onClick={closeModal}>Update</button>
+              <button onClick={
+                () => {
+                  setTemp(0.1)
+                  setMaxTok(2000)
+                  setFreq(0)
+                  setPres(0.5)
+                  closeModal()
+                }}
+              >Close</button>
+            </form>
+          </div>
+        </div>
+      )}
       <div className="main-child">
         <h3>Topic / Description</h3>
         <textarea value={topic} onChange={(e) => { setTopic(e.target.value) }} />
@@ -33,26 +152,86 @@ function BlogPostGenerator() {
         <h3>Tone of Voice (Optional)</h3>
         <input type="text" value={tone} onChange={(e) => { setTone(e.target.value) }} />
         <h3>Max Length</h3>
-        <select id="cars" name="cars">
-          <option value={2000}> Please Select </option>
-          <option value={500}> 500 words </option>
+        <select id="cars" name="cars" onChange={(handleDropdownChange)}>
+          <option value={-1}> Please Select </option>
+          <option value={500}>500 words</option>
           <option value={1000}>1000 words</option>
           <option value={1500}>1500 words</option>
           <option value={2000}>2000 words</option>
         </select>
-        <button onClick={handleWrite}>
-          <b>Write</b>
-        </button>
+
+        {/* WordPress Markdown Feature Start */}
+        <h3>Wordpress Markdown</h3>
+        <div className="flex-row">
+          <div className="flex-col">
+            <label>Include</label>
+          </div>
+          <div className="flex-col">
+            <input
+              className="checkbox"
+              type="checkbox"
+              checked={isWordPressMarkdown}
+              onChange={() => {
+                setIsWordPressMarkdown(!isWordPressMarkdown)
+              }}
+            />
+          </div>
+        </div>
+        {/* WordPress Markdown Feature End */}
+        <div className="btn-row">
+          <button onClick={handleWrite} className={contentIsWriting && "disabled"}>
+            {contentIsWriting ? <b>Writing...</b> : <b>Write</b>}
+          </button>
+          <button className="settings-btn" onClick={openModal}><span>ai settings</span></button>
+        </div>
+
       </div>
       <div className="main-child">
         <h3>Content</h3>
-        <textarea className="content" value={content} />
-        <button>
+        {contentIsLoading
+          ?
+          <div className="loading"><div className="loading-spinner"></div></div>
+          :
+          <textarea ref={textareaRef} className="content" value={content} />
+        }
+        <button onClick={async () => {
+          try {
+            await navigator.clipboard.writeText(content);
+          } catch (error) {
+            console.error("Failed to copy to clipboard: ", error);
+          }
+
+        }}>
           <b>Copy to clipboard</b>
         </button>
+
+        {/* Save As Text Feature Start */}
+        <button onClick={() => {
+          const text = content;
+          const filename = "gpt-result.txt";
+          const blob = new Blob([text], { type: "text/plain;charset=utf-8" });
+
+          if (window.navigator.msSaveOrOpenBlob) {
+            // for Internet Explorer
+            window.navigator.msSaveOrOpenBlob(blob, filename);
+          } else {
+            // for other browsers
+            const elem = window.document.createElement("a");
+            elem.href = window.URL.createObjectURL(blob);
+            elem.download = filename;
+            document.body.appendChild(elem);
+            elem.click();
+            document.body.removeChild(elem);
+          }
+        }}><b>Save as txt</b></button>
+
+        {/* Save As Text Feature End */}
       </div>
     </div>
   );
 }
 
 export default BlogPostGenerator;
+
+
+
--- blog/src/ai.js
+++ blog/src/ai.js
@@ -1,35 +1,13 @@
-const { Configuration, OpenAIApi } = require("openai");
 const api_key = "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"
 
-// export class OpenAI {
-//     constructor() {
-//         // Create the Configuration and OpenAIApi instances
-//         this.openai = new OpenAIApi(new Configuration({ api_key }));
-//     }
-//     // Asynchronous function to generate text from the OpenAI API
-//     async generateText(prompt, model, max_tokens, temperature = 0.85) {
-//         try {
-//             // Send a request to the OpenAI API to generate text
-//             const response = await this.openai.createCompletion({
-//                 model,
-//                 prompt,
-//                 max_tokens,
-//                 n: 1,
-//                 temperature,
-//             });
-//             console.log(`request cost: ${response.data.usage.total_tokens} tokens`);
-//             // Return the text of the response
-//             console.log(response.data.choices[0].text)
-//             return response.data.choices[0].text;
-//         } catch (error) {
-//             throw error;
-//         }
-//     }
-// }
+export async function generate(prompt, obj) {
 
+    if (obj) {
+        console.log("Obj exists: ", obj)
+    } else {
+        console.log("No Object")
+    }
 
-export async function generate(prompt) {
-    // let prompt = "Write a blog post about machine learning in the tone of a pirate"
     let res = "None"
     const requestOptions = {
         method: 'POST',
@@ -39,23 +17,21 @@ export async function generate(prompt) {
         },
         body: JSON.stringify({
             'prompt': prompt,
-            'temperature': 0.1,
-            'max_tokens': Math.floor(2000),
+            'temperature': obj ? obj.temperature : 0.1,
+            'max_tokens': obj ? obj.max_tokens : Math.floor(2000),
             'top_p': 1,
-            'frequency_penalty': 0,
-            'presence_penalty': 0.5,
+            'frequency_penalty': obj ? obj.frequency_penalty : 0,
+            'presence_penalty': obj ? obj.presence_penalty : 0.5,
             'stop': ["\"\"\""],
         })
     };
     await fetch('https://api.openai.com/v1/engines/text-davinci-003/completions', requestOptions)
         .then(response => response.json())
         .then(data => {
-            // Do something with data
-            //console.log(data.choices[0].text)
             res = data.choices[0].text
         }).catch(err => {
             console.log("Ran out of tokens for today! Try tomorrow!");
-            res = "Ran out of tokens for today! Try tomorrow!"
+            res = "Ran out of tokens"
         });
     return res
 }
